73d0bbf2a3a21909dd32e01244ce0506f789c9a0
fernando <fernando_lopezc@live.com>
2019-03-07 21:39:55 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -77,15 +77,14 @@ def count_contestants_by_hometown(data, hometown)
   contestants_from_hometown
   #binding.pry
 end
-
-
-
-
-
-
+=begin
+Build a method get_occupation, that takes in two arguments––the data hash and a string of a hometown. 
+It returns the occupation of the first contestant who hails from that hometown.
+=end
 def get_occupation(data, hometown)
-  # code here
+
 end
+get_occupation(data,"Cranston, Rhode Island") #> "Journalist"
 
 def get_average_age_for_season(data, season)
   # code here
